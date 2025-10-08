d457a8992f3758314820966aae8f75501962e76b
Lars <lars.wijntjes@gmail.com>
2025-10-08 22:34:41 +0200

    Add safeguard against users accidentally nesting the mod

--- Nopelatro.lua
+++ Nopelatro.lua
@@ -407,4 +407,13 @@ SMODS.current_mod.config_tab = function()
     }}
 end
 
+-- safe-guard against nested folders (unapplied lovely patches)
+local lovely_toml_info = NFS.getInfo(SMODS.current_mod.path .. "lovely.toml")
+local lovely_dir_items = NFS.getInfo(SMODS.current_mod.path .. "lovely") and NFS.getDirectoryItems(SMODS.current_mod.path .. "lovely")
+local should_have_lovely = lovely_toml_info or (lovely_dir_items and #lovely_dir_items > 0)
+if should_have_lovely then
+    -- if we have detected a `lovely.toml` file or a non-empty `lovely` directory (assumption that it contains lovely patches)
+    assert(SMODS.current_mod.lovely, "Failed to detect Nopelatro lovely patches.\n\nPlease make SURE your Nopelatro folder is NOT nested (it should look like Mods/Nopelatro/<files>, not Mods/Nopelatro/Nopelatro/<files>).\n\n\n\n")
+end
+
 sendDebugMessage("Nopelatro loaded", "nopelatro")
